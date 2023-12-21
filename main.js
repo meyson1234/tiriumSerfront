ee69f873a0e01068a0226afd2f3a444eea1d1ab0
meyson1234 <143870392+meyson1234@users.noreply.github.com>
2023-12-21 14:16:03 +0300

    time

--- main.js
+++ main.js
@@ -26,14 +26,16 @@ window.addEventListener('DOMContentLoaded',()=>{
 
             // console.log(t.waitTime)
             fullObjLocal.value = output;
-            if(!btn.classList.contains('active')){
-           const time = setTimeout(() => {
+            
+            let time = setTimeout(() => {
+             if(!btn.classList.contains('active')){
             console.log(count++)
               if(!btn.classList.contains('active')) console.log(time);
                 textStatus.innerText = 'Упал'
                 btn.innerText = 'Старт';
                 btn.classList.remove('active');
-            }, t.waitTime);}
+              }
+            }, t.waitTime);
 
 
         const fetcher = async () => {
@@ -48,7 +50,7 @@ window.addEventListener('DOMContentLoaded',()=>{
             });
             const content = await rawResponse;
             console.log(content);
-            return content
+            return content;
           };
 
           fetcher()
@@ -58,7 +60,7 @@ window.addEventListener('DOMContentLoaded',()=>{
           .then((data) => {
             if(data.indexOf('Ожидание сброшено.') !== -1) {
                 btn.innerText = 'Старт';
-                // clearTimeout(time);
+                clearTimeout(time);
                 // console.log('time')
                 btn.classList.remove('active');
             } else {
@@ -70,71 +72,3 @@ window.addEventListener('DOMContentLoaded',()=>{
         
             })
 })
-
-    // const start = (minuf,textf,urlinputLocf) => {
-        
-    //     textStatus.innerHTML = "бегает!";
-    //     textStatus.style.background = 'burlywood'
-        
-    //     const minu = isValidIdo(minuf),
-    //           text = isValidIdo(textf),
-    //           urlinputLoc = isValidIdo(urlinputLocf);
-    //     console.log(minuf)
-    //     const now = new Date(),
-    //           minutes = Math.floor((minu / (1000 * 60)) % 60);
-        
-    //     // console.log(typeof objStri);
-
-    //         interval =  setInterval(function () {
-    //             clearInterval(interval);
-                
-              
-    //             const url = `https://api.telegram.org/bot6447470353:AAEgx88L_vRqPhi6y_edY0Te1S7aSL6k9yQ/sendMessage?chat_id=6377248808&text=Упал Тест: ${text} %0A %0Aдата и время: ${now} %0A %0AУрл на тест: ${urlinputLoc} %0AВремя ожидания прогона: ${minu} млс ${minutes !== 0?'≈ ' + minutes + ' мин':''}`
-    //             fetch(url);
-
-    //             textStatus.innerHTML = "провален!";
-    //             textStatus.style.background = '#d36a6a';
-                
-    //             fullObj.removeAttribute('disabled');
-
-    //             btn.classList.remove('active');
-    //             btn.innerHTML = 'Старт';
-    //         },minu)
-
-    //     }
-
-    // const stop = () => {
-    //         clearInterval(interval);
-    //         textStatus.innerHTML = "успешно!";
-    //         textStatus.style.background = '#2aa568'
-    //     }
-  // if(!btn.classList.contains('active')){
-
-            //     btn.innerHTML = 'Стоп'
-            //     btn.classList.add('active');
-
-            //     const {minu,text,urlinputLoc} = objStri;
-
-            //     start(
-            //         minu,
-            //         text,
-            //         urlinputLoc
-            //         );
-                
-            //         fullObjLocal.setAttribute('disabled',true);
-            //         fullObjLocal.value = '';
-          
-            // } else {
-            //     fullObjLocal.value = '';
-            //     fullObjLocal.removeAttribute('disabled');
-
-            //     btn.innerHTML = 'Старт'
-            //     btn.classList.remove('active');
-            //     stop();
-            // }
-
-
-// const string = '{"waitTime" : 300000,"text": "[prod] [entry_points]  smog проверка точки входа из км товара в цыкле","urlinputLoc" :"https://th.yandex-team.ru/test-case/MARKETFRONT-101/edit"}';
-// const b = '"id":'+213123214123412+',' 
-// var output = [string.slice(0, 1), b, string.slice(1)].join('');
-// console.log(output);
